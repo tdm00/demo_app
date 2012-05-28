5b6bbdea0c137b35759b9c50eaaa7310044ebb08
Troy Murray <troy.murray@gmail.com>
2012-05-28 00:40:00 -0400

    Fix mass-assignment problem wtih functional tests on user model

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: @user.attributes
+      post :create, user: { name: @user.name, email: @user.email }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -35,7 +35,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    put :update, id: @user, user: @user.attributes
+    put :update, id: @user, user: { name: @user.name, email: @user.email }
     assert_redirected_to user_path(assigns(:user))
   end
 
